157eca27b30bc13bf4d8a9728b079daf5c1f20d1
FrancuzProg <36542724+FrancuzProg@users.noreply.github.com>
2018-02-16 23:15:24 +0700

    Add array sorting algorithms
    3rd and 4th labs.

--- AlgorithmsLabs/StartForm.Designer.cs
+++ AlgorithmsLabs/StartForm.Designer.cs
@@ -127,6 +127,7 @@
             this.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.Name = "StartForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Лабораторные";
             this.ResumeLayout(false);
             this.PerformLayout();
